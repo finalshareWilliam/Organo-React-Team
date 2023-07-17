3d8e45b38a8594af1ebfb608c5170f8b9c97580d
William Souza <william.h.souza@hotmail.com>
2023-07-17 18:50:24 -0300

    v2.0

---
+++ src/componentes/Banner/index.js
@@ -0,0 +1,7 @@
+import './baner.css';
+const Banner = () => {
+    return (<header className="cabecalho">
+        <img src='/imagens/banner.png' alt='Logo do Organo'/>
+    </header>)
+}
+export default Banner
